4c4f13d526bc536510497b5d643e9e38d24fe57d
RLoad <wurui19930213@gmail.com>
2023-05-15 17:01:50 +0800

    add bifurcation func in keyboard

--- include/robot_control/keyboard.h
+++ include/robot_control/keyboard.h
@@ -14,6 +14,8 @@
 #include <stdio.h>
 #include <termios.h>
 
+#include "Eigen/Eigen"
+
 // namespace keyboard {
 class Keyboard {
  public:
@@ -27,6 +29,15 @@ class Keyboard {
 
 };  // class Keyboard
 // }  // namespace keyboard
+
+float* findVelocities(float data[], int M, int N, int startPnts[], int K, float dt);
+float* removeFinalDatasample(float data[], int M, int N, int startPnts[], int K);
+float* cart2hyper(float data[], int M, int N);
+float* hyper2cart(float hyper[], int M, int N);
+float* cart2sphvel(float data[], float hyper[], float vel[], int M, int N, int startPnts[], int K);
+float* sph2cartvel(float hyper[], float sphVel[], int M, int N);
+Eigen::Matrix3f eul2rotmat(float x, float y, float z);
+Eigen::Vector4f eul2quat(float yaw, float pitch, float roll);
 
 
 #endif  // QP_CONTROLLERS_EXAMPLE_KEYBOARD_H
